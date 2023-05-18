379b3025389ccd4013cf01acf7ddcf09e4ef2f5e
Leo-nI-D <leon.levchuk@gmail.com>
2023-05-18 03:34:36 +0300

    added Class Brimmy

--- js/index.js
+++ js/index.js
@@ -1,10 +1,21 @@
-let checkboxButton = document.getElementById('checkbox-sova');
+let checkboxButtonSova = document.getElementById('checkbox-sova');
 let sova = document.getElementById('sova');
 
-checkboxButton.onclick = function () {
+checkboxButtonSova.onclick = function () {
     if (sova.style.visibility === "visible") {
         sova.style.visibility = "hidden";
     } else {
         sova.style.visibility = "visible";
+    }
+}
+
+let checkboxButtonBrimmy = document.getElementById('checkbox-brimmy');
+let brimmy = document.getElementById('brimmy');
+
+checkboxButtonBrimmy.onclick = function () {
+    if (brimmy.style.visibility === "visible") {
+        brimmy.style.visibility = "hidden";
+    } else {
+        brimmy.style.visibility = "visible";
     }
 }
